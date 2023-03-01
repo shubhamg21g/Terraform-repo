57993489aa1cfd9f097cccc846d0cc64ef8a42de
shubham <shubham02749339@gmail.com>
2023-03-01 16:57:24 +0530

    firstday

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
-/* resource "aws_instance" "terraform" {
+resource "aws_instance" "terraform" {
   ami = "ami-0735c191cf914754d"
   instance_type = "t2.micro"
   
   tags = {
     Name = "terraform-ec2-instance"
   }
-} */
+}
